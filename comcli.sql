984a1068423eb21ff1bccd140bca2890330c108b
pstephanludus <p.stephan@ludus-academie.com>
2018-02-23 16:41:47 +0100

    Add files via upload

---
+++ comcli.sql
@@ -0,0 +1,18 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.4
+-- https://www.phpmyadmin.net/
+--
+-- Client :  127.0.0.1
+-- Généré le :  Ven 09 Février 2018 à 16:16
+-- Version du serveur :  5.7.14
+-- Version de PHP :  5.6.25
+
+INSERT INTO `client` (`NCLI`, `NOM`, `ADRESSE`, `LOCALITE`, `CAT`, `COMPTE`) VALUES
+('A420', 'Alkpote', '420 aller de l\'aiglion', 'Evry', 'a4', 50000),
+('B512', 'GILLET', '14, r. de l\'Eté', 'Toulouse', 'Ba', 870),
+('C003', 'AVRON', '8, r. de la Cure', 'Toulouse', 'B1', 1700),
+('C400', 'FERARD', '65, r. du Tertre', 'Poitiers', 'B2', 350),
+('F400', 'JACOB', '78, ch. du Moulin', 'Bruxelles', 'C2', 0),
+('K111', 'VANBIST', '180, r. Florimont', 'Lille', 'B1', 7200),
+('K729', 'NEUMAN', '40, r. Bransart', 'Toulouse', '', 0),
+('L422', 'FRANCK', '60, r. de Wépion', 'Namur', 'C1', 0);
